275721096aa0f355bc1daaf97f1e06b0325947f3
yono-ms <yono.ms@outlook.com>
2019-04-27 19:36:52 +0900

    add image control

--- ImageTest/MainPage.xaml.cs
+++ ImageTest/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -11,6 +14,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
@@ -20,11 +24,79 @@ namespace ImageTest
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         public MainPage()
         {
             this.InitializeComponent();
+
+            Loaded += async (s, e) =>
+            {
+                PathText = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+                var files = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFilesAsync();
+                files.ToList().ForEach(item => Items.Add(item.Path));
+            };
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _PathText;
+
+        public string PathText
+        {
+            get { return _PathText; }
+            set { _PathText = value; OnPropertyChanged(); }
+        }
+
+        private int _SelectedIndex = -1;
+
+        public int SelectedIndex
+        {
+            get { return _SelectedIndex; }
+            set { _SelectedIndex = value; OnPropertyChanged(); DisplayImage(value); }
+        }
+
+        private Visibility _ImageVisibility = Visibility.Collapsed;
+
+        public Visibility ImageVisibility
+        {
+            get { return _ImageVisibility; }
+            set { _ImageVisibility = value; OnPropertyChanged(); }
+        }
+
+        private ImageSource _ImageSource;
+
+        public ImageSource ImageSource
+        {
+            get { return _ImageSource; }
+            set { _ImageSource = value; OnPropertyChanged(); }
+        }
+
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+
+        private void DisplayImage(int index)
+        {
+            if (index != -1)
+            {
+                System.Diagnostics.Debug.WriteLine($"---- {Items[index]}");
+                ImageSource = new BitmapImage(new Uri(Items[index]));
+                ImageVisibility = Visibility.Visible;
+            }
+        }
+
+        public void ButtonClear()
+        {
+            ImageVisibility = Visibility.Collapsed;
+            SelectedIndex = -1;
+            // これは重要
+            ImageSource = null;
+        }
+
     }
 }
